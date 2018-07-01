51153ddff106041a3b7d286d259e21ebac2c6307
Kayla Reopelle <kreopelle@gmail.com>
2018-07-01 15:05:10 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,8 +3,8 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    binding.pry 
-    full_name = self.first_name self.last_name
+    binding.pry
+    full_name = self.first_name + " " + self.last_name
   end
 
   def list_roles
