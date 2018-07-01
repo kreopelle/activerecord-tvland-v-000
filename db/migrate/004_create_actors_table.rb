e385a7b6423e61a72dddc58a54238af08b7260f2
Kayla Reopelle <kreopelle@gmail.com>
2018-07-01 14:34:53 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors_table.rb
+++ db/migrate/004_create_actors_table.rb
@@ -1,3 +1,8 @@
 class CreateActorsTable < ActiveRecord::Migration[4.2]
-
-end 
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
+  end 
+end
