8471e0e2cdc717203c28101d35d2d2f89c2fdd0e
Kayla Reopelle <kreopelle@gmail.com>
2018-07-01 14:30:07 +0000

    automatically backed up by learn

--- db/migrate/004_add_first_and_last_name_to_actors.rb
+++ db/migrate/004_add_first_and_last_name_to_actors.rb
@@ -1,4 +1,5 @@
 class AddFirstAndLastNameToActors < ActiveRecord::Migration[4.2]
   def change
-    add_column 
+    add_column :actors, :first_name, :string
+    add_column :actors, :last_name, :string
 end
