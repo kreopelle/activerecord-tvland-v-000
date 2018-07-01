662098e8cbf952d80d40d2fe69368ad9f12d21df
Kayla Reopelle <kreopelle@gmail.com>
2018-07-01 15:28:30 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,8 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.each do |character|
-      binding.pry
+    self.characters.collect do |character|
       puts "#{character.name} - #{character.show.name}"
     end
   end
